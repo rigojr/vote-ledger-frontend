f6258754d00b7e6abf7087bcafc7bc3bad4bb903
rigojr <rigojunnior@gmail.com>
2020-04-17 11:54:10 -0400

    Plantilla de Users principal lista, sigue el CRUD y su respectivo renderizado

--- src/containers/Users/Users.js
+++ src/containers/Users/Users.js
@@ -5,11 +5,22 @@ import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Table from 'react-bootstrap/Table'
 
 import Aux from '../../hoc/Aux';
 import styles from './Users.module.css' 
 
 class User extends Component {
+
+    state = {
+        users: [
+            { id: "1", nombre: "José Salas", facultad: "Ingeniería", escuela: "Informática", email: "jsalas@gmail.com"},
+            { id: "2", nombre: "Simón Esperanza", facultad: "Ingeniería", escuela: "Informática", email: "esperanzas@gmail.com"},
+            { id: "3", nombre: "Ramón Bravo", facultad: "Ciencias Sociales", escuela: "Comunicación Social", email: "bravor@gmail.com"},
+            { id: "4", nombre: "Victoria Ramirez", facultad: "Derecho", escuela: "Derecho", email: "ramirezv@gmail.com"},
+            { id: "5", nombre: "Fernanda Chacón", facultad: "Ciencias Sociales", escuela: "Letras", email: "chacof@gmail.com"}
+        ]
+    }
 
     componentDidMount () {
         console.log("Users.js is mount")
@@ -23,7 +34,33 @@ class User extends Component {
                 <Container>
                     <Row lg={2}>
                         <Col xs lg="12">
-                            <p>User</p>
+                        <h1>Lista de Usuarios del Sistema</h1>
+                            <Table responsive>
+                                <thead>
+                                    <tr>
+                                        <th>Nombre</th>
+                                        <th>Facultad</th>
+                                        <th>Escuela</th>
+                                        <th>Email</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.users.map(
+                                            user => {
+                                                return (
+                                                    <tr key={user.id}>
+                                                        <td>{user.nombre}</td>
+                                                        <td>{user.facultad}</td>
+                                                        <td>{user.escuela}</td>
+                                                        <td>{user.email}</td>
+                                                    </tr>
+                                                )
+                                            }
+                                        )
+                                    }
+                                </tbody>
+                            </Table>
                         </Col>
                     </Row>
                 </Container>
