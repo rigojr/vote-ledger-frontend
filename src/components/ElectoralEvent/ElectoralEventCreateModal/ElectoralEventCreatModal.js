c3e13f33225a727b79b92cee538b52591c5a0ae2
rigojr <rigojunnior@gmail.com>
2020-06-09 11:57:15 -0400

    Se instaló React Date Picker, se implementarón validaciones breves en la vista de electoral evetns, así como el crear y el botener la tabla, también se integró el mensaje más el spinner ya creado.

--- src/components/ElectoralEvent/ElectoralEventCreateModal/ElectoralEventCreatModal.js
+++ src/components/ElectoralEvent/ElectoralEventCreateModal/ElectoralEventCreatModal.js
@@ -3,8 +3,11 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Aux';
+import styles from './inputElectoralEvents.module.css'
 
 const usercreatemodal = ( props ) => (
     <Aux>
@@ -12,19 +15,34 @@ const usercreatemodal = ( props ) => (
             <Form.Group as={Col}>
                 <Form.Label>Código del Evento</Form.Label>
                 <Form.Control
-                    as="input" />
+                    as="input"
+                    value={props.inputValues['eventCode']}
+                    onChange={props.setEvent}/>
             </Form.Group>
         </Row>
         <Row>
             <Form.Group as={Col}>
-                <Form.Label>Fecha de Inicio</Form.Label>
-                <Form.Control
-                    type="email" />
+                <Form.Label className={`${styles.DisplayOption}`}>Fecha de Inicio</Form.Label>
+                <DatePicker
+                    className={`${styles.DisplayOption} ${styles.inputStyle}`}
+                    onChange={props.setInitValue}
+                    selected={props.inputValues['initDate']}
+                    showTimeSelect
+                    dateFormat="Pp"
+                    minDate={new Date()}
+                    placeholderText="Seleccione una fecha de inicio"/>
             </Form.Group>
             <Form.Group as={Col}>
                 <Form.Label>Fecha de Finalización</Form.Label>
-                    <Form.Control
-                        type="password" />
+                <DatePicker 
+                    className={`${styles.DisplayOption} ${styles.inputStyle}`}
+                    onChange={props.setEndValue}
+                    selected={props.inputValues['endDate']}
+                    showTimeSelect
+                    dateFormat="Pp"
+                    minDate={props.inputValues['initDate']}
+                    disabled={!props.inputValues['initDate']}
+                    placeholderText="Seleccione una fecha de Fin"/>
             </Form.Group>
         </Row>
     </Aux>
--- src/containers/ElectoralEvent/ElectoralEvent.js
+++ src/containers/ElectoralEvent/ElectoralEvent.js
@@ -1,39 +1,111 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 
+import axios from '../../axios';
+
 import Aux from '../../hoc/Aux';
 import SubHeader from '../../components/Layout/Subheader/Subheader';
 import AllTable from '../../components/Layout/AllTable/AllTable';
 import AllModal from '../../components/Layout/Modal/AllModal';
 import CreateModal from '../../components/ElectoralEvent/ElectoralEventCreateModal/ElectoralEventCreatModal';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class ElectoralEvent extends Component {
 
     state = {
-        electoralEvents: [
-            { id: "1", estado: "Convocatoria", fechaInicio: "2020-05-05T06:00", fechaFin: "2020-05-05T20:00"},
-            { id: "2", estado: "Adjudicación", fechaInicio: "2019-05-05T06:00", fechaFin: "2019-05-05T20:00"},
-            { id: "3", estado: "Adjudicación", fechaInicio: "2018-05-05T06:00", fechaFin: "2018-05-05T20:00"}
-        ],
+        electoralEvents: null,
         theaderTable: ["Código","Estado","Inicio","Finalización", ""],
         showModal: false,
-        search: ''
+        search: '',
+        form: {
+            initDate: null,
+            endDate: null,
+            eventCode: ''
+        },
+        modalMessage: '',
+        enableState: false
     }
 
     componentDidMount () {
-        console.log("ElectoralEvents.js is mount");
-        // Here we ask for the initial data
+        axios.get('/electoral-events.json')
+        .then( response => {
+            const fetch = [];
+            for( let key in response.data){
+                fetch.push({
+                    id: response.data[key].id,
+                    state: response.data[key].state,
+                    initDate: response.data[key].initDate,
+                    endDate: response.data[key].endDate
+                });
+            }
+            this.setState({ electoralEvents: fetch });
+        })
+        .catch( error => {
+            console.log(error)
+        })
     }
 
-    createElectoralEventHandler = () => {
+    setInitDate = (date) => {
+        this.setState( prevState => ({ form:{
+            ...prevState.form,
+            initDate: date
+        } }) )
+    }
+
+    setEndDate = (date) => {
+        this.setState( prevState => ({ form:{
+            ...prevState.form,
+            endDate: date
+        } }) )
+    }
+
+    setEventCode = (e) => {
+        const eventCode = e.target.value;
+        this.setState( prevState => ({ form: {...prevState.form, eventCode } }) )
+    }
+
+    setModalMessage = (message) => {
+        console.log("setModalMessage");
+        console.log(message)
+        this.setState(  { modalMessage: message} );
+    }
+
+    cleanModalHandler = () => {
+        this.setState( { 
+            enableState: false,
+            form:{
+                initDate: null,
+                endDate: null,
+                eventCode: ''
+            }
+        } );
+    }
+
+    createHandler = () => {
         console.log("Creating New Electoral Event");
+        this.setModalMessage("Enviando información al Blockchain");
+        this.setState( { enableState: true} );
+        axios.post('/electoral-events.json', {
+            id: this.state.form.eventCode,
+            state: 'Convocatoria',
+            initDate: this.state.form.initDate,
+            endDate: this.state.form.endDate
+        })
+        .then( (response) => {
+            console.log(response);
+            this.setModalMessage("Guardado con éxito!");
+        })
+        .catch( error => {
+            console.log(error);
+        });
+        setTimeout(this.cleanModalHandler,3000);
     }
 
-    consultElectoralEventHandler = () =>{
+    consultHandler = () =>{
         console.log("Consulting Electoral Event");
     }
 
-    deleteElectoralEventHandler = () =>{
+    deleteHandler = () =>{
         console.log("Deleting Electoral Event");
     }
 
@@ -45,7 +117,7 @@ class ElectoralEvent extends Component {
     }
 
 
-    searchElectoralEventHandler = () => {
+    searchHandler = () => {
         console.log("Searching Electoral Event");
         console.log(this.state.search);
     }
@@ -57,21 +129,29 @@ class ElectoralEvent extends Component {
 
     render(){
 
-        let ElectoralEventsComponent = this.props.isAuthed ?
-        <AllTable 
-            theadArray={this.state.theaderTable}
-            payloadArray={this.state.electoralEvents}
-            consultHandler={this.consultElectoralEventHandler}
-            deleteHandler={this.deleteElectoralEventHandler}
-            deleteAction={true}/>    
+        let RedirectComponent = this.props.isAuthed ?
+            null
             :
-        <Redirect from="/Dashboard" to="/login"/>;
+        <Redirect from="/electora-events" to="/login"/>;
+
+        let ElectoralEventsComponent = <Spinner/>;
+
+        if (this.state.electoralEvents){
+            ElectoralEventsComponent = (
+                <AllTable 
+                    theadArray={this.state.theaderTable}
+                    payloadArray={this.state.electoralEvents}
+                    consultHandler={this.consultHandler}
+                    deleteHandler={this.deleteHandler}
+                    deleteAction={true}/> 
+            );
+        }
 
         return(
             <Aux>
                 <SubHeader
                     subHeaderTitle="Eventos Electorales"
-                    searchHandler={this.searchElectoralEventHandler}
+                    searchHandler={this.searchHandler}
                     btnName="Evento Electoral"
                     searchPlaceholder="Código del Evento Electoral"
                     showModal={this.modalHandler}
@@ -81,11 +161,18 @@ class ElectoralEvent extends Component {
                 <AllModal
                     showModal={this.modalHandler}
                     modalBoolean={this.state.showModal}
-                    createHandler={this.createElectoralEventHandler}
+                    createHandler={this.createHandler}
                     modalTitile="Crear Evento Electoral"
-                    create={true}>
-                    <CreateModal />
+                    create={true}
+                    enableState={this.state.enableState}
+                    modalMessage={this.state.modalMessage}>
+                    <CreateModal 
+                        setInitValue={this.setInitDate}
+                        setEndValue={this.setEndDate}
+                        setEvent={this.setEventCode}
+                        inputValues={this.state.form}/>
                 </AllModal>
+                {RedirectComponent}
             </Aux>
         )
     }
